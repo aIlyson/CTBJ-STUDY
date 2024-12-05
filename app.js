b67b9313f60054c24620902900285055f27eab86
Gabriel Campos <gabrielcampos548@gmail.com>
2024-12-04 21:03:25 -0300

    Bixo eu só quero dormir

--- app.js
+++ app.js
@@ -8,17 +8,19 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Configuração para servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/", indexRoutes);
 
+// Middleware para lidar com páginas não encontradas (404)
 app.use((req, res) => {
   res.status(404).render("404");
 });
 
 app.listen(PORT, () => {
-  console.log(`rodando em http://localhost:${PORT}`);
+  console.log(`Rodando em http://localhost:${PORT}`);
 });
